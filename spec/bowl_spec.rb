3838d75e1d0704a8b441ec8277ed3f983b2f9554
Donald Drummond <donald.e.drummond@gmail.com>
2013-11-21 13:21:33 -0600

    Added Strike feature

--- spec/bowl_spec.rb
+++ spec/bowl_spec.rb
@@ -6,22 +6,32 @@ require_relative '../bowl'
 describe BowlingGame do
 	let(:game) { BowlingGame.new }
 	describe '.initialize' do
-	
-	    it 'sets @rolls to an empty array' do
-
+	   it 'sets @rolls to an empty array' do
 	      expect(game.rolls).to eq([])
 	    end
-	end
-	describe '#score' do
-		it "scores the game a 0 when only gutter balls are thrown" do
-			20.times { game.roll(0) }
-
-			expect(game.score).to eq(0)
 		end
-		it 'scores the game as 20 when one pine is knocked down on each roll' do
-			20.times { game.roll(1) }
+	describe '#score' do
+		context 'when only gutter balls are thrown' do
+			it "scores the game a 0 when only gutter balls are thrown" do
+				20.times { game.roll(0) }
 
-			expect(game.score).to eq(20)
+				expect(game.score).to eq(0)
+			end
+	 	end
+	 context 'when one pin is knocked down with each roll' do 
+			it 'scores the game as 20 when one pine is knocked down on each roll' do
+			
+				20.times { game.roll(1) }
+
+				expect(game.score).to eq(20)
+			end
+	end
+	context 'wnen a spare is thrown' do
+	 	it 'records the frame score as 10 plus the number of pins knocked down in the next frame with the first ball' do
+	   3.times { game.roll(5) }
+	   17.times {game.roll(0) }
+
+	   	expect(game.score).to eq(20)
 		end
 	end
 end
