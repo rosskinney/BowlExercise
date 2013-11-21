765a99f037444e92c4637183d24558564f5a4df3
Donald Drummond <donald.e.drummond@gmail.com>
2013-11-21 11:22:44 -0600

    Add passing tests for initialize and score

--- spec/bowl_spec.rb
+++ spec/bowl_spec.rb
@@ -5,10 +5,19 @@ require_relative '../bowl'
 
 describe BowlingGame do
 	describe '.initialize' do
-		it 'sets @rolls to an empty array' do
-			game = BowlingGame.new
+	    it 'sets @rolls to an empty array' do
+	      game = BowlingGame.new
 
-			expect(game.rolls).to eq([])
+	      expect(game.rolls).to eq([])
+	    end
+	end
+	describe '#score' do
+		it "scores the game a 0 when only gutter balls are thrown" do
+			game = BowlingGame.new 
+			20.times { game.roll(0) }
+
+			expect(game.score).to eq(0)
 		end
 	end
 end
+		
