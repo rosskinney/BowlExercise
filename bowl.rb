e45781afbf6470b736744f1f410868698c9a4e28
MakerSquare Student <students@themakersquare.com>
2013-11-21 20:20:29 -0600

    Add strike logic to score method

--- bowl.rb
+++ bowl.rb
@@ -1,37 +1,42 @@
 class BowlingGame
-	attr_accessor :rolls
+  attr_accessor :rolls
 
-	def initialize
-		@rolls = []
-	end
-	# Record a roll in the game
-	#
-	# pins - The integer number of pins knocked down in this roll,
-	#
-	# Returns nothing
-	def roll(pins)
-		@rolls.push(pins)
-	end
+  def initialize
+    @rolls = []
+  end
 
-	def score
-		total_score = 0 
-		current_roll = 0
+  # Record a roll in the game
+  #
+  # pins - The integer number of pins knocked down in this roll,
+  #
+  # Returns nothing
+  def roll(pins)
+    @rolls.push(pins)
+  end
 
-		while current_roll < @rolls.size
-			roll = @rolls[current_roll]
-			next_roll = @rolls[current_roll + 1]
+  # Returns the Integer score for this game. Expects to be run after all rolls
+  # for the game have been recorded.
+  def score
+    total_score = 0
+    current_roll = 0
 
-			if roll + next_roll == 10
-				total_score += 10 + @rolls[current_roll + 2]
-			else
-				total_score += roll + next_roll
-			end
-			
-			current_roll += 2
+    while current_roll < @rolls.size
+      roll = @rolls[current_roll]
+      next_roll = @rolls[current_roll + 1]
 
-		end
+      if roll == 10
+        total_score += 10 + next_roll + @rolls[current_roll + 2]
+        current_roll += 1
+      elsif roll + next_roll == 10
+        total_score += 10 + @rolls[current_roll + 2]
+        current_roll += 2
+      else
+        total_score += roll + next_roll
+        current_roll += 2
+      end
+    end
 
-			return total_score
-		end
+    return total_score
+  end
 end
-	
+
